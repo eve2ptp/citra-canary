5dcec01f2997300b1fae398337c6c3c2aa25081d
citrabot <citra@citra-emu.org>
2020-01-28 15:38:39 +0000

    Merge PR 5025

--- src/citra_qt/configuration/config.cpp
+++ src/citra_qt/configuration/config.cpp
@@ -252,6 +252,8 @@ void Config::ReadCoreValues() {
     qt_config->beginGroup(QStringLiteral("Core"));
 
     Settings::values.use_cpu_jit = ReadSetting(QStringLiteral("use_cpu_jit"), true).toBool();
+    Settings::values.cpu_clock_percentage =
+        ReadSetting(QStringLiteral("cpu_clock_percentage"), 100).toInt();
 
     qt_config->endGroup();
 }
@@ -729,6 +731,8 @@ void Config::SaveCoreValues() {
     qt_config->beginGroup(QStringLiteral("Core"));
 
     WriteSetting(QStringLiteral("use_cpu_jit"), Settings::values.use_cpu_jit, true);
+    WriteSetting(QStringLiteral("cpu_clock_percentage"), Settings::values.cpu_clock_percentage,
+                 100);
 
     qt_config->endGroup();
 }
