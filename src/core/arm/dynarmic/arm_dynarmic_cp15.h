d4a89ad2f2c73fdda0fd2926b8295d87f2edd20e
citrabot <citra@citra-emu.org>
2020-03-27 22:56:56 +0000

    Merge PR 5145

--- src/core/arm/dynarmic/arm_dynarmic_cp15.h
+++ src/core/arm/dynarmic/arm_dynarmic_cp15.h
@@ -17,7 +17,7 @@ public:
     explicit DynarmicCP15(const std::shared_ptr<ARMul_State>&);
     ~DynarmicCP15() override;
 
-    boost::optional<Callback> CompileInternalOperation(bool two, unsigned opc1, CoprocReg CRd,
+    std::optional<Callback> CompileInternalOperation(bool two, unsigned opc1, CoprocReg CRd,
                                                        CoprocReg CRn, CoprocReg CRm,
                                                        unsigned opc2) override;
     CallbackOrAccessOneWord CompileSendOneWord(bool two, unsigned opc1, CoprocReg CRn,
@@ -26,10 +26,10 @@ public:
     CallbackOrAccessOneWord CompileGetOneWord(bool two, unsigned opc1, CoprocReg CRn, CoprocReg CRm,
                                               unsigned opc2) override;
     CallbackOrAccessTwoWords CompileGetTwoWords(bool two, unsigned opc, CoprocReg CRm) override;
-    boost::optional<Callback> CompileLoadWords(bool two, bool long_transfer, CoprocReg CRd,
-                                               boost::optional<u8> option) override;
-    boost::optional<Callback> CompileStoreWords(bool two, bool long_transfer, CoprocReg CRd,
-                                                boost::optional<u8> option) override;
+    std::optional<Callback> CompileLoadWords(bool two, bool long_transfer, CoprocReg CRd,
+                                               std::optional<u8> option) override;
+    std::optional<Callback> CompileStoreWords(bool two, bool long_transfer, CoprocReg CRd,
+                                                std::optional<u8> option) override;
 
 private:
     std::shared_ptr<ARMul_State> interpreter_state;
