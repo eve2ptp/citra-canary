5ab431c9ab94f3483c25b9717595ae91876bc2f3
citrabot <citra@citra-emu.org>
2022-05-26 00:25:56 +0000

    Merge PR 6025

--- src/citra_qt/main.cpp
+++ src/citra_qt/main.cpp
@@ -1958,17 +1958,18 @@ void GMainWindow::OnSaveMovie() {
 
 void GMainWindow::OnCaptureScreenshot() {
     OnPauseGame();
-    QFileDialog png_dialog(this, tr("Capture Screenshot"), UISettings::values.screenshot_path,
-                           tr("PNG Image (*.png)"));
-    png_dialog.setAcceptMode(QFileDialog::AcceptSave);
-    png_dialog.setDefaultSuffix(QStringLiteral("png"));
-    if (png_dialog.exec()) {
-        const QString path = png_dialog.selectedFiles().first();
-        if (!path.isEmpty()) {
-            UISettings::values.screenshot_path = QFileInfo(path).path();
-            render_window->CaptureScreenshot(UISettings::values.screenshot_resolution_factor, path);
-        }
+    QString path = UISettings::values.screenshot_path;
+    if (!FileUtil::IsDirectory(path.toStdString())) {
+        path = QString::fromStdString(FileUtil::GetUserPath(FileUtil::UserPath::UserDir));
+        path.append(QStringLiteral("screenshots"));
+        FileUtil::CreateFullPath(path.toStdString());
+        UISettings::values.screenshot_path = path;
     }
+    const QString filename = game_title.remove(QRegularExpression(QStringLiteral("[\\/:?\"<>|]")));
+    const QString timestamp =
+        QDateTime::currentDateTime().toString(QStringLiteral("dd.MM.yy_hh.mm.ss.z"));
+    path.append(QStringLiteral("/%1_%2.png").arg(filename).arg(timestamp));
+    render_window->CaptureScreenshot(UISettings::values.screenshot_resolution_factor, path);
     OnStartGame();
 }
 
