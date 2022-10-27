7334297282af44409e9d7398590af3db8bfd25df
citrabot <citra@citra-emu.org>
2022-10-27 00:33:39 +0000

    Merge PR 6171

--- src/core/core.h
+++ src/core/core.h
@@ -61,6 +61,7 @@ class RendererBase;
 
 namespace Core {
 
+class ExclusiveMonitor;
 class Timing;
 
 class System {
@@ -372,6 +373,8 @@ private:
     std::unique_ptr<Memory::MemorySystem> memory;
     std::unique_ptr<Kernel::KernelSystem> kernel;
     std::unique_ptr<Timing> timing;
+
+    std::unique_ptr<Core::ExclusiveMonitor> exclusive_monitor;
 
 private:
     static System s_instance;
