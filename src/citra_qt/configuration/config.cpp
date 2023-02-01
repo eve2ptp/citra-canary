3f9c2e60c79cfa34dda1c56bdd4960cf970f5a3c
citrabot <citra@citra-emu.org>
2023-02-01 00:30:19 +0000

    Merge PR 6252

--- src/citra_qt/configuration/config.cpp
+++ src/citra_qt/configuration/config.cpp
@@ -482,6 +482,7 @@ void Config::ReadLayoutValues() {
     ReadGlobalSetting(Settings::values.layout_option);
     ReadGlobalSetting(Settings::values.swap_screen);
     ReadGlobalSetting(Settings::values.upright_screen);
+    ReadGlobalSetting(Settings::values.large_screen_proportion);
 
     if (global) {
         ReadBasicSetting(Settings::values.mono_render_option);
@@ -993,6 +994,7 @@ void Config::SaveLayoutValues() {
     WriteGlobalSetting(Settings::values.layout_option);
     WriteGlobalSetting(Settings::values.swap_screen);
     WriteGlobalSetting(Settings::values.upright_screen);
+    WriteGlobalSetting(Settings::values.large_screen_proportion);
 
     if (global) {
         WriteBasicSetting(Settings::values.mono_render_option);
