7b55a4a372136686159456a6f8869271f70aaa32
citrabot <citra@citra-emu.org>
2023-03-01 00:31:05 +0000

    Merge PR 6313

--- src/core/cheats/gateway_cheat.cpp
+++ src/core/cheats/gateway_cheat.cpp
@@ -16,6 +16,11 @@
 #include "core/hle/service/hid/hid.h"
 #include "core/memory.h"
 
+#ifdef ANDROID
+#include <boost/iostreams/device/file_descriptor.hpp>
+#include <boost/iostreams/stream.hpp>
+#endif
+
 namespace Cheats {
 
 struct State {
@@ -473,11 +478,19 @@ std::string GatewayCheat::ToString() const {
 std::vector<std::unique_ptr<CheatBase>> GatewayCheat::LoadFile(const std::string& filepath) {
     std::vector<std::unique_ptr<CheatBase>> cheats;
 
+#ifdef ANDROID
+    boost::iostreams::stream<boost::iostreams::file_descriptor_source> file;
+    OpenFStream<std::ios_base::in>(file, filepath);
+    if (!file.is_open()) {
+        return cheats;
+    }
+#else
     std::ifstream file;
     OpenFStream(file, filepath, std::ios_base::in);
     if (!file) {
         return cheats;
     }
+#endif
 
     std::string comments;
     std::vector<CheatLine> cheat_lines;
